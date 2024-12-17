b1871e7fc325d482d4366f3f2879768525134226
FraReturn <francescopennacchia@libero.it>
2024-12-17 17:42:26 +0100

    Add other Helps

--- Assets/Scenes/Scripts/Menu/Menu.cs
+++ Assets/Scenes/Scripts/Menu/Menu.cs
@@ -1,10 +1,17 @@
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Menu : MonoBehaviour
 {
     public void OpenScene(string sceneName)
     {
         UnityEngine.SceneManagement.SceneManager.LoadScene(sceneName);
+    }
+
+    public void ResetScene()
+    { 
+        string currentSceneName = SceneManager.GetActiveScene().name;
+        SceneManager.LoadScene(currentSceneName);
     }
 
     public void CloseApp()
